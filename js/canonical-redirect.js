579f5b9120cd463a0a6d711edd8c83950643895a
Kenneth Kebaara <kkebaara@yahoo.com>
2025-08-24 12:07:07 -0600

    Remove canonical redirect script for best SEO compliance

--- js/canonical-redirect.js
+++
@@ -1,4 +0,0 @@
-/* Redirect /index.html to / for canonical URL enforcement */
-if (window.location.pathname.match(/\/index\.html$/)) {
-  window.location.replace(window.location.pathname.replace(/index\.html$/, ''));
-}
